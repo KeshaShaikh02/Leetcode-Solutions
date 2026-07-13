f59bd7b03d0fe07444c7c6ee4596be9445e040e1
KeshaShaikh02 <keshashaikh27@gmail.com>
2026-07-13 19:30:15 +0530

    Solved Is Subsequence

---
+++ Arrays/392_Is_Subsequence.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isSubsequence(string s, string t)
+{
+    int i = 0;
+    int j = 0;
+
+    while (i < s.length() && j < t.length())
+    {
+        if (s[i] == t[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+
+    return i == s.length();
+}
+
+int main()
+{
+    string s = "abc";
+    string t = "ahbgdc";
+
+    if (isSubsequence(s, t))
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+
+    return 0;
+}
